4a27631d3e38b67b33cf44bca10ea4cbb15836d3
Gabriele-66 <gabrielerecchia70@gmail.com>
2022-02-23 15:34:05 +0100

    remove guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,8 @@ import {LoginComponent} from './login/login.component';
 import {ChoicePageComponent} from './choice-page/choice-page.component';
 import {AdminPageComponent} from './admin-page/admin-page.component';
 import {UserPageComponent} from './user-page/user-page.component';
-import {RouteGuardGuard} from "./services/route-guard.guard";
 
 const routes: Routes = [
-  {canActivate: [RouteGuardGuard]},
   {path: "", redirectTo: "/login", pathMatch: "full"},
   {path: 'login', component: LoginComponent},
   {path: 'disambiguation', component: ChoicePageComponent},
