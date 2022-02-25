6539fe7e1e945e3c4fcc376cbadec070a3ca9217
Gabriele-66 <gabrielerecchia70@gmail.com>
2022-02-25 16:25:51 +0100

    form list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {ReactiveFormsModule} from "@angular/forms";
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -52,6 +53,7 @@ import { MenubarModule } from 'primeng/menubar';
     CardModule,
     MenubarModule,
     BrowserAnimationsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {ListService} from '../services/list.service';
 import {Product} from '../model/product';
 import {PrimeNGConfig} from 'primeng/api';
+import { Event, RouterEvent, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-list',
@@ -10,14 +12,29 @@ import {PrimeNGConfig} from 'primeng/api';
 })
 export class ListComponent implements OnInit {
   products: Product[] = [];
+  operation: string = 'Edit';
+  public currentRoute!: string;
 
   constructor(
     private listService: ListService,
-    private primengConfig: PrimeNGConfig
-  ) {}
+    private primengConfig: PrimeNGConfig,
+    public router: Router
+  ) {
+    router.events
+      .pipe(filter((e: Event): e is RouterEvent => e instanceof RouterEvent))
+      .subscribe((e: RouterEvent) => {
+        this.currentRoute = e.url;
+      });
+  }
 
   ngOnInit() {
     this.listService.getProducts().then((data) => (this.products = data));
     this.primengConfig.ripple = true;
+  }
+
+  edit() {
+    if (this.operation == 'Edit') {
+      this.operation = 'Confirm';
+    }
   }
 }
